2ec5405af6e893f70020c444ed2b0d2b3eb5a687
rishikeshekade@gmail.com <rishikeshekade@gmail.com>
2022-05-03 01:04:48 +0530

    First Commit

---
+++ Family.java
@@ -0,0 +1,22 @@
+public class Family
+{
+	public void name()
+	{
+	System.out.println("navlkishor chandak");
+	}
+	public void name1()
+	{
+	System.out.println("ankit chandak");
+	}
+
+	public void name2()
+	{
+	System.out.println("Chandak");
+	}
+
+
+	
+
+
+
+}
